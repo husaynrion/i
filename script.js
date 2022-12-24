c02ea14761442e71a164b12fb7d37e7950a3d1d0
husaynrion <58782541+husaynrion@users.noreply.github.com>
2022-12-25 00:20:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function gmail() {
+    // copying gmail address
+    var text = "husayn.buet@gmail.com";
+    navigator.clipboard.writeText(text);
+    alert("Copied the text: " +text);
+    }
+function about(){
+    // Card Appear
+    var card = document.getElementById("card");
+    card.className= "cardAppear";
+    var about = document.getElementById("about");
+    about.style.animation = "none";
+    // Set ABOUT text in the card
+    aboutText = document.getElementById("aboutText");
+    aboutText.style.display = "flex";
+    missionText = document.getElementById("missionText");
+    missionText.style.display = "none";
+    // Show Social Icons
+    document.getElementById("link").style.display = "block";
+}
+var missionBool = 0;
+function mission(){
+    // Swapping 'MISSION' & 'U.S.A.'
+    if(missionBool==0){
+        document.getElementById("mission").innerHTML = "U.S.A.";
+        missionBool =1;
+    }else{
+        document.getElementById("mission").innerHTML = "MISSION";
+        missionBool =0;
+    }
+    // Set MISSION text in the card
+    aboutText = document.getElementById("aboutText");
+    aboutText.style.display = "none";
+    missionText = document.getElementById("missionText");
+    missionText.style.display = "flex";
+    // Hide Social Icons
+    document.getElementById("link").style.display = "none";
+}
