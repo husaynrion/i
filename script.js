142e60ed9e421e46a1574c1522754df7b326ab8d
husaynrion <58782541+husaynrion@users.noreply.github.com>
2023-03-02 21:10:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -17,39 +17,4 @@ function about(){
     card.className= "cardAppear";
     var about = document.getElementById("about");
     about.style.animation = "none";
-    // Set ABOUT text in the card
-    aboutText = document.getElementById("aboutText");
-    aboutText.style.display = "flex";
-    missionText = document.getElementById("missionText");
-    missionText.style.display = "none";
-    // Show Social Icons
-    document.getElementById("link").style.display = "block";
-
-    // Getting Back Card Size, Picture and Name
-    document.getElementById("pic").style.display = "flex";
-    document.getElementById("name").innerHTML = "HOSSAIN RION";
-}
-var missionBool = 0;
-function mission(){
-    // Swapping 'MISSION' & 'U.S.A.'
-    
-    if(missionBool==0){
-        document.getElementById("mission").innerHTML = "U.S.A.";
-        missionBool =1;
-    }else{
-        document.getElementById("mission").innerHTML = "MISSION";
-        missionBool =0;
-    }
-    
-    // Set MISSION text in the card
-    aboutText = document.getElementById("aboutText");
-    aboutText.style.display = "none";
-    missionText = document.getElementById("missionText");
-    missionText.style.display = "flex";
-
-    // Removing Card Size, Picture and Name
-    document.getElementById("pic").style.display = "none";
-    document.getElementById("name").innerHTML = "Before I Die";
-    // Hide Social Icons
-    document.getElementById("link").style.display = "none";
 }
